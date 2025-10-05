697f5f8671feff6a9a493163138f999600f48a1d
Soulxyz <xygzs2018@qq.com>
2025-10-05 18:35:31 +0800

    fix: 修复macOS下缺少png文件打包失败的问题

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use std::sync::Mutex;
 use std::time::SystemTime;
-use tauri::{Manager, State};
+use tauri::{Manager, State};//get_webview_window 方法需要Manager导入，勿删
 use serde::{Deserialize, Serialize};
 
 // Windows 平台特定的导入，用于隐藏命令行窗口和处理编码
